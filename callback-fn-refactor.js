9cd28d80724e06c45c1ef0a1dbaa125723c00a15
hans kurniadi <hans.d.k@live.com>
2017-02-09 19:46:42 +0000

    refactor callback function using foreach

---
+++ callback-fn-refactor.js
@@ -0,0 +1,17 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(found);
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(arr[i], i);   // execute callback
+  //   }
+  // }
+}
+
+function actionWhenFound(person, pos) {
+  if (person == 'Waldo') {
+    console.log("Found " + person + " at index " + pos + "!");
+  }
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
